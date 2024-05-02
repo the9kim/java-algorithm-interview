2763b9ae05cdb2f0e66e1bb12563d7f41f27f22d
the9kim <the9rla@gmail.com>
2024-05-02 11:32:36 +0900

    chore: Add comments

--- src/main/java/org/javaalgorithminterview/ch18_binary_search/P75_2_Immigration.java
+++ src/main/java/org/javaalgorithminterview/ch18_binary_search/P75_2_Immigration.java
@@ -11,10 +11,11 @@ public class P75_2_Immigration {
         long answer = 0;
 
         long left = 1;
-        long right = Arrays.stream(times).max().getAsInt() * n;
+        long right = (long) Arrays.stream(times).max().getAsInt() * n;
 
         long mid = right;
 
+        // Why should this comparison operation contain equal(=)?
         while (left <= right) {
             long calcN = 0;
 
@@ -22,13 +23,14 @@ public class P75_2_Immigration {
                 calcN += (mid / time);
             }
 
+            // The answer continue to converge until the remainder is zero when dividing the total time by a unit time
             if (calcN >= n) {
                 answer = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
-
+            // Why isn't the logic to calculate mid at the beginning?
             mid = left + (right - left) / 2;
         }
         return answer;
--- src/main/java/org/javaalgorithminterview/ch20_sliding_window/P81_2_Sliding_Window_Maximum.java
+++ src/main/java/org/javaalgorithminterview/ch20_sliding_window/P81_2_Sliding_Window_Maximum.java
@@ -19,20 +19,24 @@ public class P81_2_Sliding_Window_Maximum {
         int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
+            // Add an Element onto the window
             window.add(nums[i]);
 
             if (i < k - 1) {
                 continue;
             }
 
+            // Update the max value or Calculate a new one
             if (max == Integer.MIN_VALUE) {
                 max = window.stream().max(Integer::compareTo).get();
             } else if (nums[i] > max) {
                 max = nums[i];
             }
 
+            // Add the max value onto the answer
             answer.add(max);
 
+            // Remove an element which is out of a range and initialize a max value
             if (window.poll() == max) {
                 max = Integer.MIN_VALUE;
             }
