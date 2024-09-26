9b2cfddb54221c202081a3a99d8e618f18fe2166
the9kim <the9rla@gmail.com>
2024-09-26 11:12:55 +0900

    refactor: Java algorithm interview Q14-Merge-Two-Sorted-Lists

---
+++ src/main/java/org/javaalgorithminterview/ch8_linked_list/P14_3_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,39 @@
+package org.javaalgorithminterview.ch8_linked_list;
+
+public class P14_3_Merge_Two_Sorted_Lists {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list2.val < list1.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+
+        list1.next = mergeTwoLists(list1.next, list2);
+
+        return list1;
+    }
+}
