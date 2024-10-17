1b3ebb5750c824b2a2d6cb39865cba8befe632a9
the9kim <the9rla@gmail.com>
2024-10-17 10:55:56 +0900

    refactor: Kakao23 Q1-Personal-zInformation-Collection-Validity-Period

--- src/main/java/org/kakao23/P1_3_Personal_Information_Collection_Validity_Period.java
+++ src/main/java/org/kakao23/P1_3_Personal_Information_Collection_Validity_Period.java
@@ -1,74 +1,44 @@
 package org.kakao23;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class P1_3_Personal_Information_Collection_Validity_Period {
 
-    int today;
-
     public int[] solution(String today, String[] terms, String[] privacies) {
-        this.today = convertDate(today);
+        int convertedToday = convertToDay(today);
 
-        Map<String, Integer> termMap = createTermMap(terms);
-
-        Map<Integer, Integer> expiredMap = createExpiredMap(termMap, privacies);
-
-        return findExpiredPrivacies(expiredMap);
+        return findExpiredInfo(terms, privacies, convertedToday);
     }
 
-    private int convertDate(String date) {
-        String[] dateSplit = date.split("\\.");
-        int year = Integer.parseInt(dateSplit[0]);
-        int month = Integer.parseInt(dateSplit[1]);
-        int day = Integer.parseInt(dateSplit[2]);
+    private int convertToDay(String today) {
+        String[] tSplit = today.split("\\.");
 
-        return (year - 2000) * 12 * 28 + month * 28 + day;
+        return (Integer.parseInt(tSplit[0]) - 2000) * 12 * 28
+                + (Integer.parseInt(tSplit[1]) * 28)
+                + Integer.parseInt(tSplit[2]);
     }
 
-    private Map<String, Integer> createTermMap(String[] terms) {
+    private int[] findExpiredInfo(String[] terms, String[] privacies, int convertedToday) {
         Map<String, Integer> termMap = new HashMap<>();
 
         for (String term : terms) {
-            String[] termArr = term.split(" ");
-            termMap.put(termArr[0], Integer.parseInt(termArr[1]) * 28);
+            termMap.put(term.substring(0, 1), Integer.parseInt(term.substring(2)) * 28);
         }
 
-        return termMap;
-    }
-
-    private Map<Integer, Integer> createExpiredMap(Map<String, Integer> termMap, String[] privacies) {
-
-        Map<Integer, Integer> expiredMap = new HashMap<>();
+        List<Integer> expiredInfo = new ArrayList<>();
 
         for (int i = 0; i < privacies.length; i++) {
-            String[] pSplit = privacies[i].split(" ");
-            int start = convertDate(pSplit[0]);
-            int duration = termMap.get(pSplit[1]);
+            int expireDay = convertToDay(privacies[i].substring(0, 10)) + termMap.get(privacies[i].substring(11)) - 1;
 
-            expiredMap.put(i + 1, start + duration - 1);
-        }
-
-        return expiredMap;
-    }
-
-    private int[] findExpiredPrivacies(Map<Integer, Integer> expiredMap) {
-
-        List<Integer> results = new ArrayList<>();
-
-        for (Map.Entry<Integer, Integer> entry : expiredMap.entrySet()) {
-            if (today > entry.getValue()) {
-                results.add(entry.getKey());
+            if (convertedToday > expireDay) {
+                expiredInfo.add(i + 1);
             }
         }
 
-        Collections.sort(results);
-
-        return results.stream().mapToInt(Integer::intValue).toArray();
+        return expiredInfo.stream().mapToInt(Integer::intValue).toArray();
     }
-
 
 }
