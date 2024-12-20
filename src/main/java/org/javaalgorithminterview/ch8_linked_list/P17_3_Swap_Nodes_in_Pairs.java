0f66c701ea1c4e900dd3ff38abf92e63c94cee65
the9kim <the9rla@gmail.com>
2024-12-20 09:10:11 +0900

    refactor: Java algorithm interview Q17-Swap-Nodes-in-Pairs

--- src/main/java/org/javaalgorithminterview/ch8_linked_list/P17_3_Swap_Nodes_in_Pairs.java
+++ src/main/java/org/javaalgorithminterview/ch8_linked_list/P17_3_Swap_Nodes_in_Pairs.java
@@ -9,27 +9,24 @@ public class P17_3_Swap_Nodes_in_Pairs {
     }
 
     public ListNode swapPairs(ListNode head) {
-
         if (head == null || head.next == null) {
             return head;
         }
 
         ListNode root = new ListNode();
         root.next = head;
-        ListNode node = root;
+        ListNode prev = root;
 
-        while (node != null && node.next != null) {
-            ListNode a = node.next;
-            ListNode b = node.next.next;
+        while (prev != null && prev.next != null) {
+            ListNode node = prev.next;
+            ListNode next = prev.next.next;
 
-            if (b != null) {
-                node.next = b;
-                a.next = b.next;
-                b.next = a;
-            } else {
-                node.next = a;
+            if (next != null) {
+                prev.next = next;
+                node.next = next.next;
+                next.next = node;
             }
-            node = a;
+            prev = node;
         }
 
         return root.next;
