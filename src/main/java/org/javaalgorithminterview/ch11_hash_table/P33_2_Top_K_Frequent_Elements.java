bba8f4f66aee287ad9d583b6d5076e38527a8dc7
the9kim <the9rla@gmail.com>
2025-02-02 12:52:25 +0900

    Java algorithm interview Q33-Top-K-Frequent-Elements

--- src/main/java/org/javaalgorithminterview/ch11_hash_table/P33_2_Top_K_Frequent_Elements.java
+++ src/main/java/org/javaalgorithminterview/ch11_hash_table/P33_2_Top_K_Frequent_Elements.java
@@ -1,11 +1,9 @@
 package org.javaalgorithminterview.ch11_hash_table;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public class P33_2_Top_K_Frequent_Elements {
@@ -17,13 +15,13 @@ public class P33_2_Top_K_Frequent_Elements {
             counter.put(n, counter.getOrDefault(n, 0) + 1);
         }
 
-        List<Integer> values = counter.entrySet().stream()
+        List<Integer> sortedKeys = counter.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
 
-        List<Integer> topK = values.subList(0, k);
-
-        return topK.stream().mapToInt(Integer::intValue).toArray();
+        return sortedKeys.subList(0, k).stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
