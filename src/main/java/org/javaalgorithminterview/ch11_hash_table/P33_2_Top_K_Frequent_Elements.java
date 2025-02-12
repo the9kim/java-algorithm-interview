7308791a4c761a9530f34f49aef6e0bbef9abacd
the9kim <the9rla@gmail.com>
2025-02-12 09:52:31 +0900

    Java algorithm interview Q33-Top-K-Frequent-Elements

--- src/main/java/org/javaalgorithminterview/ch11_hash_table/P33_2_Top_K_Frequent_Elements.java
+++ src/main/java/org/javaalgorithminterview/ch11_hash_table/P33_2_Top_K_Frequent_Elements.java
@@ -1,5 +1,6 @@
 package org.javaalgorithminterview.ch11_hash_table;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -9,19 +10,21 @@ import java.util.stream.Collectors;
 public class P33_2_Top_K_Frequent_Elements {
 
     public int[] topKElement(int[] nums, int k) {
+        // 1. calculate the frequency of each element using a hashmap
         Map<Integer, Integer> counter = new HashMap<>();
 
         for (int n : nums) {
             counter.put(n, counter.getOrDefault(n, 0) + 1);
         }
 
-        List<Integer> sortedKeys = counter.entrySet().stream()
+        // 2. sort the elements in descending order based on their frequency
+        int[] sortedElems = counter.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-
-        return sortedKeys.subList(0, k).stream()
                 .mapToInt(Integer::intValue)
                 .toArray();
+
+        // 3. pick the k most frequent elements and return them
+        return Arrays.copyOfRange(sortedElems, 0, k);
     }
 }
